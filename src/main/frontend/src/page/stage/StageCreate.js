c9fad1d168e805d4388c80c3f2ae22fb648cf4c6
HYUNJI830 <hyun_ji830@naver.com>
2023-06-04 17:55:49 +0900

    modify : 스테이지 생성 css 수정

--- src/main/frontend/src/page/stage/StageCreate.js
+++ src/main/frontend/src/page/stage/StageCreate.js
@@ -19,6 +19,9 @@ import Turn from "../../component/UI/stage/Turn";
 
 import Agree from "../../component/UI/stage/Agree";
 
+import Category from "../../component/UI/stage/Category";
+
+
 const StageCreate = () => {
  const token = jwtDecode(Cookies.get('Set-Cookie'));
    const uNo = token.uNo;
@@ -27,6 +30,9 @@ const StageCreate = () => {
   const [count, setCount] = useState("");
 
   const [rate, setRate] = useState("");
+
+   const [category, setCategory] = useState("");
+
   const [rateTable, setRateTable] = useState("이율표 보기");
 
   const [importTable, setImportTable] = useState([]);
@@ -40,7 +46,6 @@ const [createButton, setCreateButton] = useState(false);
  const [deposit, setDeposit] = useState("");
  const [payment, setPayment] = useState("");
 
- const [category, setCategory] = useState("");
 
 
 
@@ -55,6 +60,7 @@ const [isDuplicate, setIsDuplicate] = useState(false);
   const countHandler = (event) => {
     setCount(event.target.value);
     console.log(event.target.value);
+
   };
 
   const submitClick = () => {
@@ -68,8 +74,7 @@ const [isDuplicate, setIsDuplicate] = useState(false);
           pfRate: rate,
           deposit: deposit,
           payment : payment,
-          interest: category,
-
+          category: category,
          receiveTurn : turn,
         },
       })
@@ -95,6 +100,7 @@ const [isDuplicate, setIsDuplicate] = useState(false);
   const rateHandler = (rateData) => {
     setRate(rateData);
   };
+
 
   const importTableClick = () => {
     if (!isImportTable) {
@@ -137,9 +143,8 @@ const [isDuplicate, setIsDuplicate] = useState(false);
     setTurn(turnData);
   };
 
-  const categoryHandler = (event) => {
-    setCategory(event.target.value);
-    console.log(event.target.value);
+  const categoryHandler = (categoryData) => {
+    setCategory(categoryData);
   };
 
 
@@ -169,23 +174,27 @@ const movePage = useNavigate("");
       });
   };
 
+ const [selectedButton, setSelectedButton] = useState('');
+
+const countClickHandler = (value) => {
+    setSelectedButton(value);
+  };
+
 
 
   return (
     <>
     <div className={styles.flexD}>
-
-    <div className={styles.flex6}>
+    <div className={styles.flex2}>
       <h1 className={styles.title}> Stage 등록</h1>
-
-      <h4>
+         <h3>
         Stage 등록을 시작합니다. 선택 또는 기입한 사항을 빠짐없이 확인하여
         진행해주세요.
-      </h4>
-        </div>
-      </div>
+        </h3>
+     </div>
+     </div>
 
-    <div  className={styles.box} >
+ <div  className={styles.box} >
 
       <div  className={styles.flex2} >
        <h1 className={styles.sixth}>Stage 기본 정보 등록</h1>
@@ -195,14 +204,12 @@ const movePage = useNavigate("");
 
       <form action="/register" method="get">
       <div className={styles.flexD}>
-       <div className={styles.flex1}>
+       <div className={styles.flex2}>
 
         <div className={styles.flex2}>
-
-
-
           <h4>스테이지 이름</h4>
           <div className={styles.flexD}>
+
            <div className={styles.flex1}>
           <input className={styles.inputStage}
             style={{borderColor: isDuplicate ? 'red' : 'gray'}}
@@ -210,83 +217,87 @@ const movePage = useNavigate("");
             onChange={nameHandler}
             name="pfName"
             value={name}
-            placeholder="스테이지 이름 기재해주세요"
+            placeholder="스테이지 이름을 입력해주세요"
           />
           <div className={styles.small}>
-              <p  style={{color: isDuplicate ? 'red' : 'gray'}}>{message}</p>
+          <p  style={{color: isDuplicate ? 'red' : 'gray'}}>{message}</p>
+         </div>
 
-           </div>
-           </div>
+          </div>
 
             <div className={styles.flex1}>
-                  <button className={styles.buttonSmall} type="button" onClick={handleCheckDuplicate}>중복체크</button>
+             <button className={styles.buttonSmall} type="button" onClick={handleCheckDuplicate}>중복체크</button>
             </div>
 
             </div>
-            <div>
 
-                </div>
             </div>
-        <div className={styles.flex1}>
+
+        <div className={styles.flex2}>
         <h4>참여인원</h4>
-          <input type="radio" name="count" value="5" onClick={countHandler} />
-          5명
-          <input type="radio" name="count" value="7" onClick={countHandler} />
-          7명
+        <div className={styles.textLeft}>
+          <button type="button" name="count" value="5"  onClick={countHandler} className={styles.button1} >5명</button>
+          <button type="button" name="count" value="7"  onClick={countHandler} className={styles.button1}>7명 </button>
+
         </div>
+         </div>
 
-        <div className={styles.flex1}>
+
+        <div className={styles.flex2}>
         <h4>약정금액(총 금액)</h4>
-
+        <div className={styles.textLeft}>
           {count === "5" && (
             <>
 
-            <Participants name="deposit" value="500000"  onClick={depositHandler} >50만원(월10만원) </Participants>
-            <Participants name="deposit" value="1000000" onClick={depositHandler}>100만원(월20만원) </Participants>
-            <Participants name="deposit" value="1500000" onClick={depositHandler}>150만원(월30만원) </Participants>
-            <Participants name="deposit" value="2000000" onClick={depositHandler}>200만원(월40만원) </Participants>
-            <Participants name="deposit" value="2500000" onClick={depositHandler}>250만원(월50만원) </Participants>
+            <label  className={styles.selectLabel}><Participants name="deposit" value="500000"   onClick={depositHandler} >50만원(월10만원) </Participants> </label>
+             <label  className={styles.selectLabel}> <Participants name="deposit" value="1000000"  onClick={depositHandler}>100만원(월20만원) </Participants></label>
+             <label  className={styles.selectLabel}><Participants name="deposit" value="1500000" onClick={depositHandler}>150만원(월30만원) </Participants></label>
+
+            <label  className={styles.selectLabel}><Participants name="deposit" value="2000000" onClick={depositHandler}>200만원(월40만원) </Participants></label>
+            <label  className={styles.selectLabel}> <Participants name="deposit" value="2500000" onClick={depositHandler}>250만원(월50만원) </Participants></label>
+
             </>
           )}
           {count === "7" && (
             <>
-            <Participants name="deposit" value="700000" onClick={depositHandler}>70만원(월10만원) </Participants>
-            <Participants name="deposit" value="1400000" onClick={depositHandler}>140만원(월20만원) </Participants>
-            <Participants name="deposit" value="2100000" onClick={depositHandler}>210만원(월30만원) </Participants>
-            <Participants name="deposit" value="2800000" onClick={depositHandler}>280만원(월40만원) </Participants>
-            <Participants name="deposit" value="3500000" onClick={depositHandler}>350만원(월50만원) </Participants>
+              <label  className={styles.selectLabel}><Participants name="deposit" value="700000" onClick={depositHandler}>70만원(월10만원) </Participants></label>
+              <label  className={styles.selectLabel}><Participants name="deposit" value="1400000" onClick={depositHandler}>140만원(월20만원) </Participants></label>
+
+              <label  className={styles.selectLabel}><Participants name="deposit" value="2100000" onClick={depositHandler}>210만원(월30만원) </Participants></label>
+             <label  className={styles.selectLabel}> <Participants name="deposit" value="2800000" onClick={depositHandler}>280만원(월40만원) </Participants></label>
+            <label  className={styles.selectLabel}>  <Participants name="deposit" value="3500000" onClick={depositHandler}>350만원(월50만원) </Participants></label>
             </>
           )}
         </div>
+         </div>
 
-        <div className={styles.flex1}>
+
+        <div className={styles.flex2}>
         <h4>이율</h4>
 
+          <div className={styles.textLeft}>
           {count === "5" && (
             <>
-              <Rate name="rate" value="1.86" onClick={rateHandler}>
-                1.86%
+               <Rate name="rate" value="1.86" onClick={rateHandler}>
               </Rate>
               <Rate name="rate" value="3.97" onClick={rateHandler}>
-                3.97%
               </Rate>
             </>
           )}
           {count === "7" && (
             <>
               <Rate name="rate" value="2.65" onClick={rateHandler}>
-                2.65%
               </Rate>
               <Rate name="rate" value="7.6" onClick={rateHandler}>
-                7.6%
               </Rate>
             </>
           )}
-        </div>
 
+        </div>
+     </div>
      </div>
 
-<div className={styles.flex2}>
+        <div className={styles.flex2}>
         <div className={styles.flex2}>
           <input
             type="button"
@@ -332,90 +343,54 @@ const movePage = useNavigate("");
 
 
 
-        <div className={styles.flex1}>
+         <div className={styles.flex2}>
         <h4>나의 순번</h4>
+         <div className={styles.textLeft}>
           {count === "5" && (
             <>
-          <Turn name="turn" value="1" onClick={turnHandler}>1번 </Turn>
-          <Turn name="turn" value="2" onClick={turnHandler}>2번 </Turn>
-          <Turn name="turn" value="3" onClick={turnHandler}>3번 </Turn>
-         <Turn name="turn" value="4" onClick={turnHandler}>4번 </Turn>
-          <Turn name="turn" value="5" onClick={turnHandler}>5번 </Turn>
+         <Turn name="turn" value="1" onClick={turnHandler}> </Turn>
+          <Turn name="turn" value="2" onClick={turnHandler}> </Turn>
+          <Turn name="turn" value="3" onClick={turnHandler}></Turn>
+          <div>
+         <Turn name="turn" value="4" onClick={turnHandler}></Turn>
+          <Turn name="turn" value="5" onClick={turnHandler}> </Turn>
+          </div>
             </>
           )}
           {count === "7" && (
             <>
-          <Turn name="turn" value="1" onClick={turnHandler}>1번 </Turn>
-          <Turn name="turn" value="2" onClick={turnHandler}>2번 </Turn>
-          <Turn name="turn" value="3" onClick={turnHandler}>3번 </Turn>
-         <Turn name="turn" value="4" onClick={turnHandler}>4번 </Turn>
-          <Turn name="turn" value="5" onClick={turnHandler}>5번 </Turn>
-         <Turn name="turn" value="6" onClick={turnHandler}>6번 </Turn>
-        <Turn name="turn" value="7" onClick={turnHandler}>7번 </Turn>
+          <Turn name="turn" value="1" onClick={turnHandler}></Turn>
+          <Turn name="turn" value="2" onClick={turnHandler}> </Turn>
+          <Turn name="turn" value="3" onClick={turnHandler}> </Turn>
+          <div>
+         <Turn name="turn" value="4" onClick={turnHandler}> </Turn>
+          <Turn name="turn" value="5" onClick={turnHandler}> </Turn>
+         <Turn name="turn" value="6" onClick={turnHandler}> </Turn>
+        <Turn name="turn" value="7" onClick={turnHandler}> </Turn>
+        </div>
             </>
           )}
         </div>
+       </div>
 
 
-         <div className={styles.flex1}>
+         <div className={styles.flex2}>
           <h4>관심사</h4>
-          <input
-            type="radio"
-            name="category"
-            value="목돈"
-            onChange={categoryHandler}
-          />
-          목돈
-          <input
-            type="radio"
-            name="category"
-            value="여행"
-            onChange={categoryHandler}
-          />
-          여행
-          <input
-            type="radio"
-            name="category"
-            value="전자제품"
-            onChange={categoryHandler}
-          />
-          전자제품
-          <input
-            type="radio"
-            name="category"
-            value="패션잡화"
-            onChange={categoryHandler}
-          />
-          패션잡화
-          <input
-            type="radio"
-            name="category"
-            value="취미"
-            onChange={categoryHandler}
-          />
-          취미
-          <input
-            type="radio"
-            name="category"
-            value="웨딩"
-            onChange={categoryHandler}
-          />
-          웨딩
-          <input
-            type="radio"
-            name="category"
-            value="자동차"
-            onChange={categoryHandler}
-          />
-          자동차
+        <div className={styles.textLeft}>
+         <Category  name="category" value="목돈" onClick={categoryHandler}> </Category>
+        <Category  name="category" value="여행" onClick={categoryHandler}> </Category>
+        <Category  name="category" value="전자제품" onClick={categoryHandler}> </Category>
+        <div>
+        <Category  name="category" value="패션잡화" onClick={categoryHandler}> </Category>
+        <Category  name="category" value="취미" onClick={categoryHandler}> </Category>
+        <Category  name="category" value="웨딩" onClick={categoryHandler}> </Category>
+        <Category  name="category" value="자동차" onClick={categoryHandler}> </Category>
+        </div>
+         </div>
+
         </div>
 
-    </div>
-        </div>
-
- <div className={styles.flex2}>
-
-            <div className={styles.flex1}>
+           <div className={styles.flex1}>
              <Link to={`/stageAgree/${name}`}>
              <button className={styles.button}  onClick={()=>{
              submitClick()
@@ -423,8 +398,9 @@ const movePage = useNavigate("");
             </Link>
             </div>
 
+    </div>
 
-         </div>
+    </div>
       </form>
 
     </div>
