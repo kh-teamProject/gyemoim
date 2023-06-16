2c50222746877d7862542ac49bdec8b5ac2a5b24
e-u-gene <hahahoho2856@gmail.com>
2023-06-16 16:07:35 +0900

    atc : 머지하면서 변동된 사항 중요X

--- src/main/frontend/src/page/Test.js
+++ src/main/frontend/src/page/Test.js
@@ -1,10 +1,16 @@
 import {useEffect, useState} from "react";
 import axios from "axios";
 import { useParams} from "react-router-dom";
+import {useSelector} from "react-redux";
+import jwtDecode from "jwt-decode";
+import Cookies from "js-cookie";
 
 const Test = () => {
   const {pfID} = useParams();
   const [message, setMessage] = useState([]);
+
+  //로그인 여부 체크
+  const checkedLogin = useSelector((state) => state.checkedLogin);
 
   useEffect(() => {
       axios.get(`/test`, {
@@ -14,11 +20,13 @@ const Test = () => {
       })
         .then((res) => {
           setMessage(res.data);
+          console.log(message);
         })
         .catch((error) => {
           console.log(error);
         });
   }, []);
+
 
   return (
     <>
@@ -28,6 +36,7 @@ const Test = () => {
         <li key={index}>
           {/*계모임 식별번호: {value.pfID} |  약정금 : {value.deposit}*/}
           수령순서 : {value.receiveTurn}
+          {value.uno}
         </li>
       ))}
     </ul>
